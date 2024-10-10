fdea3a409f99ac3b1e846055216afec700932af5
joselo7990 <josemachicote@hotmail.com>
2024-10-10 15:12:34 -0300

    gil

--- back/src/db.config.js
+++ back/src/db.config.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 
-const MONGO_URI = "mongodb://127.0.0.1:27017/twitter";
+// const MONGO_URI = "mongodb://127.0.0.1:27017/twitter";
+const MONGO_URI =
+  "mongodb+srv://jose:josejose@cluster0.tsase.mongodb.net/twitter?retryWrites=true&w=majority&appName=Cluster0";
 const connectDB = async () => {
   try {
     await mongoose.connect(MONGO_URI);
